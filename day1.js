7918205b59cb9247ec087515d1cc2a8eb67234d4
HASHIM NOOR <hashimn901@gmail.com>
2024-07-13 16:51:43 +0500

    done with day one challenge

---
+++ day1.js
@@ -0,0 +1,91 @@
+var number = 1000
+console.log(number)
+
+let value = "Hashim Noor Qureshi"
+console.log(value)
+
+const isTrue = false
+console.log(isTrue)
+
+const num = 12345
+console.log(typeof (num))
+
+const boolval = false
+console.log(typeof (boolval))
+
+let name = "Hashim"
+console.log(typeof (name))
+
+
+let person = {
+  name: "Hashim Noor",
+  semester: 4,
+  section: "4A Morning"
+}
+
+console.log(typeof (person))
+
+const arrayData = ["hahsim", "yasir", "hadi"]
+console.log(typeof (arrayData))
+
+let initialVal = 1120
+
+console.log(initialVal)
+
+initialVal = 1200
+console.log(initialVal)
+
+
+const lastValue = 12000
+
+console.log(lastValue)
+
+// lastValue = 1500
+console.log(lastValue)
+
+
+const val = 1000
+console.log(`${val} and type of val is : ${typeof (val)}`)
+
+
+let data = true
+
+console.log(`${data} and type of data is ${typeof (data)}`)
+
+
+const array = ['image1', 'image2', 'image3']
+
+console.log(`${array} and its type is ${typeof (array)}`)
+
+
+var newData = "Hashim Noor"
+console.log(`${newData} and its type is ${typeof (newData)}`)
+
+let nullval = null
+console.log(`${nullval} and its type is ${typeof (nullval)}`)
+
+
+let undefined
+
+console.log(`${undefined} and its type is ${typeof (undefined)}`)
+
+let object = {
+  name: "hashim",
+  marks: 65
+}
+
+console.log(`${object.name} and its type is ${typeof (object)}`)
+
+
+const Name = "Hashim Noor"
+
+console.log(Name)
+
+// Name = "Hashim Queshi"
+console.log(Name)
+
+let newName = "Hashim Noor Qureshi"
+
+console.log(newName)
+newName = "Muhammad Hashim"
+console.log(newName)
